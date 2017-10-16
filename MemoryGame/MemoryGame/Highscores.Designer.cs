e225d6e48bed87993c54873f61bc1c69d30c08a8
Aletta104 <hoek1019@student.nhl.nl>
2017-10-16 13:03:14 +0200

    zelfde fout als eerder vanochtend

--- MemoryGame/MemoryGame/Highscores.Designer.cs
+++ MemoryGame/MemoryGame/Highscores.Designer.cs
@@ -37,7 +37,7 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(32, 35);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(131, 238);
             this.listBox1.TabIndex = 0;
@@ -46,7 +46,7 @@
             // 
             this.listBox2.FormattingEnabled = true;
             this.listBox2.Location = new System.Drawing.Point(178, 35);
-            this.listBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(2);
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(131, 238);
             this.listBox2.TabIndex = 1;
@@ -55,7 +55,7 @@
             // 
             this.button_menu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button_menu.Location = new System.Drawing.Point(356, 246);
-            this.button_menu.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_menu.Margin = new System.Windows.Forms.Padding(2);
             this.button_menu.Name = "button_menu";
             this.button_menu.Size = new System.Drawing.Size(75, 27);
             this.button_menu.TabIndex = 2;
@@ -71,7 +71,7 @@
             this.Controls.Add(this.button_menu);
             this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Highscores";
             this.Text = "Memory Game";
             this.ResumeLayout(false);
