1cd9db95ddd95563395c18ddc6c7cdaee475bbbb
Aletta104 <hoek1019@student.nhl.nl>
2017-10-30 15:25:06 +0100

    nieuwe feature!
    de basis voor een nieuwe feature, namelijk verschillende thema's. Zodra ik Moha en de andere gasten niet meer in mijn hoofd hoor, zal ik er meer code aan toevoegen

---
+++ MemoryGame/MemoryGame/Thema.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace MemoryGame
+{
+    partial class Thema
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Pick = new System.Windows.Forms.Label();
+            this.Avengers = new System.Windows.Forms.Button();
+            this.Social = new System.Windows.Forms.Button();
+            this.DisneyJunior = new System.Windows.Forms.Button();
+            this.Cars = new System.Windows.Forms.Button();
+            this.Royalty = new System.Windows.Forms.Button();
+            this.Winx = new System.Windows.Forms.Button();
+            this.buttons = new System.Windows.Forms.Button();
+            this.Disney = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Pick
+            // 
+            this.Pick.AutoSize = true;
+            this.Pick.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pick.Location = new System.Drawing.Point(144, 51);
+            this.Pick.Name = "Pick";
+            this.Pick.Size = new System.Drawing.Size(330, 25);
+            this.Pick.TabIndex = 0;
+            this.Pick.Text = "Pick a theme to play the game";
+            this.Pick.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Pick.Click += new System.EventHandler(this.Pick_Click);
+            // 
+            // Avengers
+            // 
+            this.Avengers.Location = new System.Drawing.Point(43, 99);
+            this.Avengers.Name = "Avengers";
+            this.Avengers.Size = new System.Drawing.Size(92, 38);
+            this.Avengers.TabIndex = 1;
+            this.Avengers.Text = "Avengers";
+            this.Avengers.UseVisualStyleBackColor = true;
+            // 
+            // Social
+            // 
+            this.Social.Location = new System.Drawing.Point(162, 158);
+            this.Social.Name = "Social";
+            this.Social.Size = new System.Drawing.Size(92, 38);
+            this.Social.TabIndex = 3;
+            this.Social.Text = "Formule 1";
+            this.Social.UseVisualStyleBackColor = true;
+            // 
+            // DisneyJunior
+            // 
+            this.DisneyJunior.Location = new System.Drawing.Point(43, 158);
+            this.DisneyJunior.Name = "DisneyJunior";
+            this.DisneyJunior.Size = new System.Drawing.Size(92, 38);
+            this.DisneyJunior.TabIndex = 4;
+            this.DisneyJunior.Text = "GTA 5";
+            this.DisneyJunior.UseVisualStyleBackColor = true;
+            // 
+            // Cars
+            // 
+            this.Cars.Location = new System.Drawing.Point(162, 99);
+            this.Cars.Name = "Cars";
+            this.Cars.Size = new System.Drawing.Size(92, 38);
+            this.Cars.TabIndex = 4;
+            this.Cars.Text = "Cars";
+            this.Cars.UseVisualStyleBackColor = true;
+            // 
+            // Royalty
+            // 
+            this.Royalty.Location = new System.Drawing.Point(401, 99);
+            this.Royalty.Name = "Royalty";
+            this.Royalty.Size = new System.Drawing.Size(92, 38);
+            this.Royalty.TabIndex = 5;
+            this.Royalty.Text = "Royalty";
+            this.Royalty.UseVisualStyleBackColor = true;
+            // 
+            // Winx
+            // 
+            this.Winx.Location = new System.Drawing.Point(283, 99);
+            this.Winx.Name = "Winx";
+            this.Winx.Size = new System.Drawing.Size(92, 38);
+            this.Winx.TabIndex = 5;
+            this.Winx.Text = "Winx";
+            this.Winx.UseVisualStyleBackColor = true;
+            // 
+            // buttons
+            // 
+            this.buttons.Location = new System.Drawing.Point(283, 158);
+            this.buttons.Name = "buttons";
+            this.buttons.Size = new System.Drawing.Size(92, 38);
+            this.buttons.TabIndex = 6;
+            this.buttons.Text = "buttons";
+            this.buttons.UseVisualStyleBackColor = true;
+            // 
+            // Disney
+            // 
+            this.Disney.Location = new System.Drawing.Point(401, 158);
+            this.Disney.Name = "Disney";
+            this.Disney.Size = new System.Drawing.Size(92, 38);
+            this.Disney.TabIndex = 7;
+            this.Disney.Text = "Disney Princesses";
+            this.Disney.UseVisualStyleBackColor = true;
+            // 
+            // Thema
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 248);
+            this.Controls.Add(this.Disney);
+            this.Controls.Add(this.buttons);
+            this.Controls.Add(this.Winx);
+            this.Controls.Add(this.Royalty);
+            this.Controls.Add(this.Cars);
+            this.Controls.Add(this.DisneyJunior);
+            this.Controls.Add(this.Social);
+            this.Controls.Add(this.Avengers);
+            this.Controls.Add(this.Pick);
+            this.Name = "Thema";
+            this.Text = "Thema";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Pick;
+        private System.Windows.Forms.Button Avengers;
+        private System.Windows.Forms.Button Social;
+        private System.Windows.Forms.Button DisneyJunior;
+        private System.Windows.Forms.Button Cars;
+        private System.Windows.Forms.Button Royalty;
+        private System.Windows.Forms.Button Winx;
+        private System.Windows.Forms.Button buttons;
+        private System.Windows.Forms.Button Disney;
+    }
+}
---
+++ MemoryGame/MemoryGame/Thema.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MemoryGame
+{
+    public partial class Thema : Form
+    {
+        public Thema()
+        {
+            InitializeComponent();
+        }
+
+        private void Pick_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
